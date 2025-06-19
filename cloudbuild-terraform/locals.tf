3d7624dbb15f96d80960d4768def65ac30079ad2
Michael Act <86778470+michaelact@users.noreply.github.com>
2025-06-19 14:35:53 +0700

    revert: libs cloudbuild triggers

--- cloudbuild-terraform/locals.tf
+++ cloudbuild-terraform/locals.tf
@@ -6,6 +6,19 @@ locals {
   }
 
   # Discover module names by listing directories in python/ that match the pattern
+  libs_versions      = ["3.11", "3.12"]
+  libs_modules_dirs  = distinct(flatten([for _, v in fileset("${path.module}/../libs", "**") : regex("([^/]*).*", dirname(v))]))
+  libs_modules_names = slice(local.libs_modules_dirs, 1, length(local.libs_modules_dirs))
+  libs_combined = flatten([
+    for module in local.libs_modules_names : [
+      for version in local.libs_versions : {
+        module  = module
+        version = version
+      }
+    ]
+  ])
+
+  # Discover module names by listing directories in python/ that match the pattern
   python_versions      = ["3.11", "3.12", "3.13"]
   python_modules_dirs  = distinct(flatten([for _, v in fileset("${path.module}/../python", "**") : regex("([^/]*).*", dirname(v))]))
   python_modules_names = slice(local.python_modules_dirs, 1, length(local.python_modules_dirs))

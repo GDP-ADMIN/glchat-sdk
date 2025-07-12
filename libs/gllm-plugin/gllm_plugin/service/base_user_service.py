296a737ca9b5c10fb3c22b5ea076128d4d6dfba1
Hermes <hermes.v.gani@gmail.com>
2025-07-12 12:52:04 +0700

    Add base user service class
    Introduces BaseUserService as an abstract class for user-related operations, including an abstract method to retrieve user ID from a FastAPI request. Also adds an empty __init__.py to the service module.

---
+++ libs/gllm-plugin/gllm_plugin/service/base_user_service.py
@@ -0,0 +1,21 @@
+"""Provides a base class for user service.
+
+Authors:
+    Immanuel Rhesa (immanuel.rhesa@gdplabs.id)
+"""
+
+from abc import ABC, abstractmethod
+
+from fastapi import Request
+
+
+class BaseUserService(ABC):
+    """A base class for user service."""
+
+    @abstractmethod
+    def get_user_id(self, request: Request) -> str:
+        """Abstract method to get the user id.
+
+        Returns:
+            str: user id.
+        """

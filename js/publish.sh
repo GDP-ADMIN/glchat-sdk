2685fcbbfb0e7a34d28c148aecf3cc6f5163706c
Michael Act <michael.a.c.tulenan@gdplabs.id>
2025-07-31 01:17:50 +0700

    [GCB] Unpublish GitHub Logs (#71)

--- js/publish.sh
+++ js/publish.sh
@@ -4,9 +4,7 @@ set -e
 # Check if at least one package folder name is provided
 if [ "$#" -le 0 ]; then
   echo "Error: Missing required argument package_name"
-  echo "Usage: ./build.ci.sh <package_name> <package_version>"
-  echo "- package_version must follow semantic version PEP440, default (0.0.0)."
-  echo "  https://peps.python.org/pep-0440/"
+  echo "Usage: ./publish.sh <package_name>"
   exit 1
 fi
 
@@ -14,6 +12,7 @@ MODULE="$1"
 cd "$MODULE"
 
 if [ -n "${TAG_NAME}" ]; then
+    # Publish to npm public registry
     TAG_NAME=$(echo "${TAG_NAME}" | sed -E "s/^.*-v//")
     # Check if version contains 'b' (beta)
     if [[ "$TAG_NAME" == *"b"* ]]; then

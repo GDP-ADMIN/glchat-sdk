e31446b8f9302ce23ccb9becb35d44407ea14535
Ryan Ignatius Hadiwijaya <ryan.i.hadiwijaya@gdplabs.id>
2025-08-16 15:58:09 +0700

    chore(GL Chat Plugin): add search search type (#76)

--- python/glchat-plugin/glchat_plugin/config/constant.py
+++ python/glchat-plugin/glchat_plugin/config/constant.py
@@ -7,6 +7,7 @@ References:
     None
 """
 
+import warnings
 from enum import StrEnum
 
 
@@ -15,11 +16,26 @@ class SearchType(StrEnum):
 
     Attributes:
         NORMAL: Get answer from chatbot knowledge.
-        WEB: Get more relevant information from the web.
+        SEARCH: Get answer from various connectors.
+        WEB: Get more relevant information from the web. (DEPRECATED)
             Web Search uses real-time data. Agent selection isn't available in this mode.
         DEEP_RESEARCH: Get answer from Deep Research Agent.
     """
 
     NORMAL = "normal"
-    WEB = "web"
+    SEARCH = "search"
+    _WEB = "web"  # Underscore to hide it from normal use
     DEEP_RESEARCH = "deep_research"
+
+    @property
+    def WEB(cls) -> "SearchType":
+        """Deprecated: Use SEARCH instead.
+
+        Will be removed in version 0.3.0
+        """
+        warnings.warn(
+            "SearchType.WEB is deprecated and will be removed in a future version. Use SearchType.SEARCH instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return cls._WEB
